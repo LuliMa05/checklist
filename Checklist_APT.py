5a42fcef119e67d681d584d7b796c43c283bd32c
LuliMa05 <LMsosa@lomanegra.com>
2025-07-11 08:56:56 -0300

    Add files via upload
    Subo checklist inicial

---
+++ Checklist_APT.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+from fpdf import FPDF
+
+st.title("APT")
+
+st.mardown("## datos del ejecutante de la tarea")
+nombre = st.tx_input("Nombre y apellido")
+area = st.selectbox("area", ["prod", "mant", "mpr", "ing"]
+fecha = st.date_input("fecha de ejecucion de la tarea")
+
+st.markdown("## factores de riesgos identificados")
+riesgos = {
+    "trab altura": st.checkbox("trabj en alt"),
+    "izaje": st.checkbox("izaje"),
+    "bloqueos": st.checkbox("bloqueo"),
+}
+
+comentarios = st.text_area("coment adicionales")
+
+if st.button("enviar checklist"):
+    df = pd.DataFrame([{
+        "nombre": nombre,
+        "area": area,
+        "fecha": fecha.strftime("%Y-%m-%d"),
+        **riesgos,
+        "comentarios": comentarios
+        }])
+
+    dt.to_csv("respuestas.csv", mode='a'. header=False, index=False)
+
+    pdf = FDPF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt="Checklist de Peligros Críticos", ln=True, align='C')
+    pdf.ln(10)
+
+    pdf.cell(200, 10, txt=f"Nombre: {nombre}", ln=True)
+    pdf.cell(200, 10, txt=f"Área: {area}", ln=True)
+    pdf.cell(200, 10, txt=f"Fecha: {fecha.strftime('%Y-%m-%d')}", ln=True)
+    pdf.ln(5)
+
+    for clave, valor in riesgos.items():
+        pdf.cell(200, 10, txt=f"{clave}: {'Sí' if valor else 'No'}", ln=True)
+
+    pdf.ln(5)
+    pdf.multi_cell(200, 10, txt=f"Comentarios: {comentarios}")
+    nombre_archivo_pdf = f"checklist_{nombre.replace(' ', '_')}_{fecha.strftime('%Y%m%d')}.pdf"
+    pdf.output(nombre_archivo_pdf)
+
+    st.success("Checklist enviado y PDF generado.")
+
