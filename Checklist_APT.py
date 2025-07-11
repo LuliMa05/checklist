e29d9770e767b88c2118cb008bb2a66b45d056c3
LuliMa05 <LMsosa@lomanegra.com>
2025-07-11 09:44:05 -0300

    Update Checklist_APT.py

--- Checklist_APT.py
+++ Checklist_APT.py
@@ -28,7 +28,7 @@ if st.button("enviar checklist"):
         "comentarios": comentarios
         }])
 
-df.to_csv("respuestas.csv", mode='a', header=False, index=False)
+    df.to_csv("respuestas.csv", mode='a', header=False, index=False)
 
 pdf = FDPF()
 pdf.add_page()
